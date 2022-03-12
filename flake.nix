328bb4e9f53b3c3d6f423a0140377c62d3c4b0f6
hugosenari <hugosenari@gmail.com>
2022-03-12 15:27:21 -0300

    chore: re index stomp

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A pure-nim implementation of the STOMP protocol for machine messaging.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."stomp-master".url = "path:./master";
+  inputs."stomp-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."stomp-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
